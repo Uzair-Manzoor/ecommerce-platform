5c76ee412e0ed18db336186c1bcfa81c6ba19a9e
Uzair-Manzoor <uzair.kayani555@gmail.com>
2024-05-15 15:46:19 +0500

    Create ProductDetails component to show detailed product information

---
+++ src/components/ProductDetails.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../redux/cartSlice';
+
+const ProductDetails = () => {
+  const { id } = useParams();
+  const [product, setProduct] = useState({});
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    axios.get(`/api/products/${id}`)
+      .then(response => setProduct(response.data))
+      .catch(error => console.error('Error fetching product:', error));
+  }, [id]);
+
+  const handleAddToCart = () => {
+    dispatch(addToCart(product));
+  };
+
+  return (
+    <div className="product-details">
+      <img src={product.image} alt={product.name} />
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p>${product.price}</p>
+      <button onClick={handleAddToCart}>Add to Cart</button>
+    </div>
+  );
+};
+
+export default ProductDetails;
